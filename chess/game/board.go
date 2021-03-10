8f4150773e5db242c5031088df470bef9d2721f0
DatTVu <dtvutrong1993@gmail.com>
2021-03-10 22:44:20 +0700

    chore: clean up projects

--- chess/game/board.go
+++ chess/game/board.go
@@ -1,9 +1,22 @@
 package game
 
-var board [8][8]Player
+type Board struct {
+	board [8][8]Player
+}
+
+//var board [8][8]Player
 
 const boardSize int = 8
 
-func canMove(p Player, pos BoardPos) bool {
-	return board[pos.Row][pos.Col] != p
+func (b Board) canMove(p Player, pos BoardPos) bool {
+	return b.board[pos.Row][pos.Col] != p
+}
+
+func (b *Board) CreateBoard() bool {
+	return false
+}
+
+func (b *Board) LoadBoardSettings(path string) bool {
+
+	return true
 }
