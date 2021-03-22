9df5c54721c2ee2a1bf955a5f354fbd32f6b91b0
Dat Vu <dat.vt26@viettel.com.vn>
2021-03-22 17:33:10 +0700

    feat: implement dynamic array

--- data_structure/vector/vector.go
+++ data_structure/vector/vector.go
@@ -3,35 +3,144 @@ package vector
 import (
 	"log"
 	"sync"
+	"errors"
 )
 
 const kDefaultCapacity = 16
 
 type Vector struct {
-	kCapacity int
-	kSize     int
 	kData     []int
 	mutex     sync.Mutex
 }
 
+func NewVector() *Vector {
+	return &Vector{kData: make([]int, 0, kDefaultCapacity)}
+}
+
+func NewVector(capacity int) (*Vector, error){
+	if (capacity % 2 != 0){
+		return nil, errors.New("[ERROR] Please use power of two for capacity")
+	}
+	return &Vector{kData: make([]int, 0, capacity)}, nil
+}
+
 func (vec Vector) Size() int {
-	return vec.kSize
+	vec.mutex.Lock()
+	defer vec.mutex.Unlock()
+	return len(vec.kData)
 }
 
 func (vec Vector) Capacity() int {
-	return vec.kCapacity
+	vec.mutex.Lock()
+	defer vec.mutex.Unlock()
+	return cap(vec.kData)
 }
 
 func (vec Vector) Empty() bool {
-	if vec.kSize == 0 {
+	vec.mutex.Lock()
+	defer vec.mutex.Unlock()
+	if len(vec.kData) == 0 {
 		return true
 	} else {
 		return false
 	}
 }
 
-func (vec Vector) At(idx int) int {
-	if idx > vec.kSize || idx > vec.kCapacity {
-		log.Panic("What T")
+func (vec Vector) At(idx int) (int, error) {
+	vec.mutex.Lock()
+	defer vec.mutex.Unlock()
+	if idx > len(vec.kData) || idx > cap(vec.kData) {
+		return nil, errors.New("[ERROR] Out of Range!")
+	} else{
+		return vec.kData[idx], nil
 	}
+}
+
+func (vec Vector) Push(val int) {
+	vec.mutex.Lock()
+	defer vec.mutex.Unlock()
+	if(len(vec.kData >= cap(vec.kData))){
+		vec.resize()
+	}
+	vec.kData[len(vec.kData)-1] = val
+}
+
+func (vec Vector) Insert(val int, idx int) (error){
+	vec.mutex.Lock()
+	defer vec.mutex.Unlock()
+	if (val<0){
+		return errors.New("[ERROR] Out of Range!")
+	}
+	if(len(vec.kData>=cap(vec.kData))){
+		vec.resize()
+	}
+	for i:= len(vec.kData ); i> idx; i--{
+		vec.kData[i] = vec.kData[i-1]
+	}
+
+	vec.kData[idx] = val
+	return nil
+}
+
+func (vec Vector) Prepend(val int) error{ 
+	vec.mutex.Lock()
+	defer vec.mutex.Unlock()
+	return vec.Insert(val, 0)
+}
+
+func (vec Vector) Pop() (int, error){
+	vec.mutex.Lock()
+	defer vec.mutex.Unlock()
+	if(len(vec.kData) == 0){
+		return nil, errors.New("[ERROR] Out of range!")
+	}
+	val:= vec.kData[len(vec.kData)-1]
+	vec.kData = vec.kData[:len(vec.kData)-2]
+	return val, nil
+}
+
+func (vec Vector) Delete(idx int) (error){
+	vec.mutex.Lock()
+	defer vec.mutex.Unlock()
+	if(idx < 0 || idx >= len(vec.kData) || idx >= cap(vec.kData))	{
+		return errors.New("[ERROR] Out of range!")
+	}
+	for i := idx; i< len(vec.kData) -2; i++{
+		vec.kData[i] = vec.kData[i+1]
+	}
+	vec.kData = vec.kData[:len(vec.kData) -2]
+	return nil
+}
+
+func (vec Vector) Remove(val int) error{
+	vec.mutex.Lock()
+	defer vec.mutex.Unlock()
+	for idx, v := range vec.kData{
+		if (v == val){
+			vec.Delete(idx)
+			return nil
+		}
+	}
+	return errors.New("[ERROR] Can't find value in the vector")
+}
+
+func (vec Vector) Find(val int) (int, error){
+	vec.mutex.Lock()
+	defer vec.mutex.Unlock()
+	for idx, v := range vec.kData{
+		if(v == val){
+			return idx, nil
+		}
+	}
+	return -1, errors.New("[ERROR] Can't find value in the vector")
+}
+
+func (vec Vector) resize(){
+	vec.mutex.Lock()
+	defer vec.mutex.Unlock()
+	temp := make([]int, 0, 2*cap(vec.kData))
+	for idx, val := range vec.kData{
+		temp[idx] = val
+	}
+	vec.kData := temp[:]
 }
